87147977d46890cc5e6602465ece1386eb7a3338
A K <hello@deeda.com>
2016-09-20 03:08:15 -0700

    initial commit

---
+++ blub-userAccountPlusSettings/SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  blub-userAccountPlusSettings
+//
+//  Created by Atif Khan on 9/20/16.
+//  Copyright © 2016 Telaport Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var dismissButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dismissButton.layer.cornerRadius = dismissButton.frame.size.width / 2 
+    }
+    
+    @IBAction func dismissSecondVC(_ sender: AnyObject) {
+    
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
